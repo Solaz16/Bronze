e84d9f7d1c6f7058b1b6ed36ef80e533f264432e
Solaz16 <Solaz16@users.noreply.github.com>
2026-07-09 12:28:06 +0200

    Corrige le chargement du style

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,7 @@ if (!isset($titre_page)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($titre_page) ?></title>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <header>
